be457750b6429b4ce9da025c90c5a4fa2ce5a6f4
Cyrus <markcyrus.serrano@gmail.com>
2025-09-06 10:00:31 +0800

    initialization

---
+++ src/LinkedList.java
@@ -0,0 +1,9 @@
+// File: LinkedList.java
+public class LinkedList {
+  Node head; // The first node in the list. Also called the head.
+
+  // Constructor
+  public LinkedList() {
+    this.head = null; // An empty list has a null head
+  }
+}
